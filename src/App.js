d24dd0a77b1904cad35c0fe64a5d14a97bb9b7bd
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-31 20:32:38 +0300

    Created footer with contact info

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './udacity.png';
+import udacity from './udacity.png';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+
+
+// library.add(faLinkedin, faEnvelope)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <img class="udacity-logo" src={logo} alt=""></img>
+          <img className="udacity-logo" src={udacity} alt=""></img>
           <h1 id="name">Evangelos Athanasakis</h1>
           <h2 id="knowledge">Front end developer | Physicist</h2>
         </header>
+        <footer>
+          <FontAwesomeIcon icon={faLinkedin} />
+          <a className="contact-info" href="https://www.linkedin.com/in/evangelos-athanasakis/">linkedin </a>
+          <FontAwesomeIcon icon={faGithub} />
+          <a className="contact-info" href="https://github.com/V-for-Vaggelis">github </a>
+          <FontAwesomeIcon icon={faEnvelope} />
+          <a className="contact-info" href="mailto:vaggelis.atha1993@gmail.com">vaggelis.atha1993@gmail.com </a>
+        </footer>
       </div>
-    );
+      );
+    }
   }
-}
 
-export default App;
+  export default App;
