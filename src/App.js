7b5eebbcee226b0e07b36ccb0762ea38543e60cc
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-07-31 18:32:58 +0300

    Created header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './udacity.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <img class="udacity-logo" src={logo} alt=""></img>
+          <h1 id="name">Evangelos Athanasakis</h1>
+          <h2 id="knowledge">Front end developer | Physicist</h2>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
