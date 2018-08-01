6b1000fea2ea595bf2becc970b62639f2897cef8
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-02 02:50:58 +0300

    Handle yes seperately

--- src/components/Dialogue.js
+++ src/components/Dialogue.js
@@ -5,33 +5,46 @@ import { Button, ButtonGroup } from 'react-bootstrap';
 class Dialogue extends Component {
   state = {
     previousDialogue: [],
-    dialogueActive: true
+    dialogueActive: true,
+    lastAnserYes: false
   }
   terminateDialogue = () => {
     this.setState(() => ({
       dialogueActive: false
     }))
   }
+  handleYes = () => {
+    this.setState(() => ({
+      lastAnserYes: true
+    }))
+  }
   render() {
     return (
-      <div id="dialogue">
+      <section id="dialogue">
       <p>{messages[this.props.messageCount].positive}</p>
       {this.props.messageCount < 6 &&
         <ButtonGroup>
-        <Button onClick={() => this.props.nextQuestion()}>Yes</Button>
+        <Button onClick={() => {
+          this.handleYes()
+          this.props.nextQuestion()}}>Yes</Button>
         <Button onClick={() => this.props.nextQuestion()}>No</Button>
         <Button onClick={() => this.terminateDialogue()}>Skip dialogue</Button>
         </ButtonGroup>
       }
+      {this.state.lastAnserYes &&
+        <section id="single-project">
+        Then I guess you will love this project:
+        <h2>A single project goes here</h2>
+        </section>
+      }
       {!this.state.dialogueActive &&
-        <section>Straight to the point then, here is all my work as a front-end developer!
+        <section id="all-projects">Straight to the point then, here is all my work as a front-end developer!
         <h1>Projects go here</h1>
         </section>
-
       }
-        </div>
-      );
-    }
+      </section>
+    );
   }
+}
 
-  export default Dialogue;
+export default Dialogue;
