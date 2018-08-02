c25d0580fa74a7383b555c066006bad6303cd236
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-02 15:35:15 +0300

    Implemented all of dialogue functionality

--- src/components/Dialogue.js
+++ src/components/Dialogue.js
@@ -4,10 +4,17 @@ import { Button, ButtonGroup } from 'react-bootstrap';
 
 class Dialogue extends Component {
   state = {
-    previousDialogue: [],
+    previousQuestions: [],
     dialogueActive: true,
     lastAnserYes: false,
     commonGround: false
+  }
+  showProperMessage = () => {
+    let question;
+    (((this.props.messageCount === 2 || this.props.messageCount === 5 || this.props.messageCount === 6) && this.state.commonGround === false) ?
+    question = messages[this.props.messageCount].allNegative :
+    question = messages[this.props.messageCount].positive)
+    return question;
   }
   terminateDialogue = () => {
     this.setState(() => ({
@@ -15,47 +22,54 @@ class Dialogue extends Component {
     }))
   }
   handleYes = () => {
-    this.setState(() => ({
+    let message = this.showProperMessage()
+    this.setState((prevState) => ({
       commonGround: true,
-      lastAnserYes: true
+      lastAnserYes: true,
+      previousQuestions: prevState.previousQuestions.concat([{question: message, answer: "Yes", project: "A project goes here"}])
     }))
   }
   handleNo = () => {
-    this.setState(() => ({
-      lastAnserYes: false
+    let message = this.showProperMessage()
+    this.setState((prevState) => ({
+      lastAnserYes: false,
+      previousQuestions: prevState.previousQuestions.concat([{question: message, answer: "No"}])
     }))
   }
   render() {
     return (
       <section id="dialogue">
-      <p>{
-        ((this.props.messageCount === 2 || this.props.messageCount === 5 || this.props.messageCount === 6) && this.state.commonGround === false) ?
-        messages[this.props.messageCount].allNegative : messages[this.props.messageCount].positive}</p>
-      {this.props.messageCount < 6 &&
+        {this.state.previousQuestions.map((q) => (
+          <section key={q.question}><span>{q.question}</span> <span>{q.answer}</span>
+          {(q.project) &&
+            <div>
+              Then I guess you will love this project:
+              <h2>{q.project}</h2>
+            </div>
+          }
+        </section>
+      ))
+    }
+      <p>{this.showProperMessage()}</p>
+      {(this.props.messageCount < 6 && this.state.dialogueActive) &&
         <ButtonGroup>
-        <Button onClick={() => {
-          this.handleYes()
-          this.props.nextQuestion()}}>Yes</Button>
-        <Button onClick={() => {
-          this.handleNo()
-          this.props.nextQuestion()}}>No</Button>
-        <Button onClick={() => this.terminateDialogue()}>Skip dialogue</Button>
-        </ButtonGroup>
-      }
-      {this.state.lastAnserYes &&
-        <section id="single-project">
-        Then I guess you will love this project:
-        <h2>A single project goes here</h2>
+          <Button onClick={() => {
+              this.handleYes()
+              this.props.nextQuestion()}}>Yes</Button>
+            <Button onClick={() => {
+                this.handleNo()
+                this.props.nextQuestion()}}>No</Button>
+              <Button onClick={() => this.terminateDialogue()}>Skip dialogue</Button>
+            </ButtonGroup>
+          }
+          {!this.state.dialogueActive &&
+            <section id="all-projects">Straight to the point then, here is all my work as a front-end developer!
+              <h1>Projects go here</h1>
+            </section>
+          }
         </section>
-      }
-      {!this.state.dialogueActive &&
-        <section id="all-projects">Straight to the point then, here is all my work as a front-end developer!
-        <h1>Projects go here</h1>
-        </section>
-      }
-      </section>
-    );
+      );
+    }
   }
-}
 
-export default Dialogue;
+  export default Dialogue;
