e608e7981387f3c99d7b118f113fe8d44e629af7
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-04 15:27:27 +0300

    Created project component

---
+++ src/components/Project.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+function Project (props) {
+  return (
+    <section class="project-container">
+    <h3><a href={props.project.link}>{props.project.title}</a></h3>
+    <p>{props.project.description}</p>
+    <p><em>Skills used:</em> {props.project.skillsUsed}</p></section>
+  )
+}
+
+export default Project
