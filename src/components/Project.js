317d8275fab88aa88c9ace48775b272257c323a7
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-05 16:47:38 +0300

    Added some styles for project-container

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 function Project (props) {
   return (
     <section className="project-container">
-    <h3><a href={props.project.link}>{props.project.title}</a></h3>
-    <p>{props.project.description}</p>
-    <p><em>Skills used:</em> {props.project.skillsUsed}</p></section>
+    <h3><a className="project-link" href={props.project.url}>{props.project.title}</a></h3>
+    <p><em>Desctiption:</em>  {props.project.description}</p>
+    <p><em>Skills used:</em>  {props.project.skillsUsed}</p>
+    </section>
   )
 }
 
