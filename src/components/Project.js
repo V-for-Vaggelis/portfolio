85c7aae6d342b5ba7c4ecd9e3965b56ca3a7105f
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-08 21:15:23 +0300

    Display used skills with list

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,7 +5,11 @@ function Project (props) {
     <section className="project-container" aria-label="Container of a project">
     <h3><a className="project-link" href={props.project.url}>{props.project.title}</a></h3>
     <p><em>Desctiption:</em>  {props.project.description}</p>
-    <p><em>Skills used:</em>  {props.project.skillsUsed}</p>
+    <p><em>Skills used: </em></p>
+    <ul className="skillList">{props.project.skillsUsed.map((skill) => (
+        <li key={skill}>{skill}</li>
+      ))}
+    </ul>
     </section>
   )
 }
