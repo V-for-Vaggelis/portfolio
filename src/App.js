6528795bf6ac841355f09f73467109e1a62e5661
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-11 17:26:37 +0300

    made my picture round

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
-import portrait from './graduation.JPG'
+import portrait from './graduation.png'
 import Dialogue from './components/Dialogue.js'
 import projects from './projects.json';
 import Project from './components/Project.js'
