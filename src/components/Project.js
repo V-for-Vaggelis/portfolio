3e42a54ead215e793d465357ac03e6c85e6e1d88
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-08-11 16:21:26 +0300

    Added some styles to the cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,14 +4,15 @@ import {Image} from 'react-bootstrap'
 function Project (props) {
   return (
     <section className="project-container" aria-label="Container of a project">
-    <h3><a className="project-link" href={props.project.url}>{props.project.title}</a></h3>
+    <h3>{props.project.title}</h3>
     <Image className="project-thumb" responsive src={props.project.thumbnail.src} alt={props.project.thumbnail.alt}></Image>
-    <p><em>Description:</em>  {props.project.description}</p>
+    <p>{props.project.description}</p>
     <p><em>Skills used: </em></p>
     <ul className="skillList">{props.project.skillsUsed.map((skill) => (
         <li key={skill}>{skill}</li>
       ))}
     </ul>
+    <p><em>Github link: </em><a className="project-link" href={props.project.url}>{props.project.title}</a></p>
     </section>
   )
 }
